f1d6de9f8518ccafb99723697430707987c2af7f
Ward Nakchbandi <hotrod.master@hotmail.com>
2022-12-11 13:23:05 +0300

    sync

--- nvidia-vaapi-driver/nvidia-vaapi-driver-0.0.7/debian/env-scripts/nvidia-vaapi-env.sh
+++ nvidia-vaapi-driver/nvidia-vaapi-driver-0.0.7/debian/env-scripts/nvidia-vaapi-env.sh
@@ -5,15 +5,23 @@
 nvgpu=$(lspci | grep -iE 'VGA|3D' | grep -i nvidia | cut -d ":" -f 3)
 nvkernmod=$(lspci -k | grep -iEA3 '^[[:alnum:]]{2}:[[:alnum:]]{2}.*VGA|3D' | grep -iA3 nvidia | grep -i 'kernel driver' | grep -iE 'vfio-pci|nvidia')
 
-if [[ ! -z $nvgpu ]]; then
-	if [[ ! -z $nvkernmod ]]; then
-        	export LIBVA_DRIVER_NAME=nvidia
-        	export MOZ_DISABLE_RDD_SANDBOX=1
-        	export EGL_PLATFORM=$XDG_SESSION_TYPE
-		fi
-	else
-        	echo "No NVIDIA Driver detected. No env vars set for va-api."
-	fi
+apply_env() {
+if [[ ! -z $nvkernmod ]]
+then
+echo "NVIDIA Driver detected. setting env-v vars for va-api."
+export LIBVA_DRIVER_NAME=nvidia
+export MOZ_DISABLE_RDD_SANDBOX=1
+export EGL_PLATFORM=$XDG_SESSION_TYPE
 else
-	echo "No NVIDIA GPU detected. No env vars set for va-api."
+echo "No NVIDIA Driver detected. No env vars set for va-api."
+fi
+}
+
+
+if [[ ! -z $nvgpu ]]
+then
+echo "NVIDIA GPU detected. Checking for NVIDIA Driver."
+apply_env
+else
+echo "No NVIDIA GPU detected. No env vars set for va-api."
 fi
